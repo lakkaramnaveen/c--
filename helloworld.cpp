d73a629abd82ce5fd29778af9ced3babaf1da6b8
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2026-01-11 09:37:49 -0600

    c++ learning

---
+++ helloworld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+
+    // Print Hello, World! to the console
+
+    /*
+        This is a multi-line comment.
+        The following line prints a greeting message.
+    */
+    std::cout << "I love biryani!" << std::endl;
+    std::cout << "How are you doing today!";
+    return 0;
+}
