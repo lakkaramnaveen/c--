e84759ded5648c348ea0aad8a2c8f2556f0e35b3
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2026-01-12 14:28:39 -0600

    typedef, increment, dec

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,10 +2,26 @@
 #include <vector>
 
 // custom type definition for a vector of pairs (string, int)
-typedef std::string text_t;
+// typedef std::string text_t;
+// typedef int number_t;
+
+using text_t = std::string;
+using number_t = int;
 
 int main() {
     text_t greeting = "Hello, World!";
-    std::cout << greeting << std::endl;
+    number_t year = 2024;
+    std::cout << greeting << " The year is " << year << "." << std::endl;
+
+    year++;
+    year+=1;
+    year--;
+    year-=1;
+    year*=1;
+    year/=1;
+    year%=1;
+    year += 2024;
+    std::cout << "Next year will be " << year + 1 << "." << std::endl;
+
     return 0;
 }
